0b2bdd864b28240f690def36304b65a2cc136cc4
nowitworks <joao.pinto.silveira@gmail.com>
2017-03-13 17:17:34 +0000

    Initial commit

---
+++ src/main/java/com/thisteam2nd/game2048/App.java
@@ -0,0 +1,13 @@
+package com.thisteam2nd.game2048;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
