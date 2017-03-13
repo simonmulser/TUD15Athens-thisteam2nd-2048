e59453f7cc1b37fa4d783f95b177afed61e7cee3
Simon Mulser <simonmulser@gmail.com>
2017-03-13 17:26:53 +0100

    implement jframe

--- src/main/java/com/thisteam2nd/game2048/App.java
+++ src/main/java/com/thisteam2nd/game2048/App.java
@@ -1,13 +1,16 @@
 package com.thisteam2nd.game2048;
 
-/**
- * Hello world!
- *
- */
+import javax.swing.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        JFrame game = new JFrame();
+        game.setTitle("this team second - 2048");
+        game.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        game.setSize(340, 400);
+        game.setVisible(true);
     }
 }
