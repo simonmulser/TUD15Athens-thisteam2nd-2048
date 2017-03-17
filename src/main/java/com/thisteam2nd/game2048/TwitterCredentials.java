0bbda6bdcbd8e2955b8143d653835c5b58786310
Paal Kristian Minne <paal.kristian.minne@gmail.com>
2017-03-17 12:21:32 +0100

    Add twitter observer. Horrible big commit. Sorry

---
+++ src/main/java/com/thisteam2nd/game2048/TwitterCredentials.java
@@ -0,0 +1,16 @@
+package com.thisteam2nd.game2048;
+
+/**
+ * Insert your credentials here
+ */
+public class TwitterCredentials {
+
+    public static String getOAuthConsumerKey() { return ""; }
+    public static String getOAuthConsumerSecret() { return ""; }
+    public static String getOAuthAccessToken() { return ""; }
+    public static String getOAuthAccessTokenSecret() { return ""; }
+    public static String getUser() { return "pkminne"; }
+    public static String getMessage(int score) { return ""; }
+
+}
+
